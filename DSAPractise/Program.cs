ccd998d0099fefe32cec279f5a944e9c91541666
Selvakumar S <selvakumars@cnxsi.com>
2023-08-09 09:07:06 +0530

    Stacks

--- DSAPractise/Program.cs
+++ DSAPractise/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSAPractise.Stacks;
 
 namespace DSAPractise
 {
@@ -14,7 +15,7 @@ namespace DSAPractise
         static void Main(string[] args)
         {
             //List<int> intList;
-            //#region Sorting
+            #region Sorting
 
             //#region Insertion Sort
             //var r = InsertionSort.InsertionSortOnText("11 0 1 2 0");
@@ -25,7 +26,7 @@ namespace DSAPractise
             //intList.PrtintAllItems("Insertion Sort On List:");
             //#endregion
 
-            //#endregion
+            #endregion
 
             #region Linked List
             //ListNode A = new ListNode(16);
@@ -44,9 +45,8 @@ namespace DSAPractise
             //    temp = temp.next;
             //}
             //list = SinglyLL.SortList(list);
-            #endregion
 
-            //#region LRU cache
+            #region LRU cache
             //var ss = "50 6 S 6 13 S 15 15 S 3 6 G 4 S 2 6 S 12 13 S 2 9 S 11 9 S 6 7 G 5 G 13 S 10 4 G 6 G 6 G 8 G 14 G 10 S 5 2 G 9 S 4 1 G 11 G 1 S 4 7 S 11 12 S 12 1 S 10 2 G 3 G 10 S 14 13 S 1 10 G 1 G 9 G 1 G 11 G 13 S 14 15 G 11 S 12 14 S 9 2 G 12 G 3 S 3 12 S 8 7 S 14 11 S 7 13 S 9 4 S 11 11 G 6 G 4 S 5 3";
             //ss = "6 1 S 2 1 S 2 2 G 2 S 1 1 S 4 1 G 2";
             //ss = "32 4 S 5 13 S 9 6 S 4 1 G 4 S 6 1 S 8 11 G 13 G 1 S 12 12 G 10 S 15 13 S 2 13 S 7 5 S 10 3 G 6 G 10 S 15 14 S 5 12 G 5 G 7 G 15 G 5 G 6 G 10 S 7 13 G 14 S 8 9 G 4 S 6 11 G 9 S 6 12 G 3";
@@ -79,9 +79,17 @@ namespace DSAPractise
             ////solution.set(1, 1);
             ////solution.set(4, 1);
             ////Console.WriteLine(solution.get(2));
-            //#endregion
+            #endregion
+            #endregion
 
-            var x = DSAPractise.Tree.Tree.BuildTreeFromInorderAndPreorder(new List<int>() { 1,2,3 }, new List<int>() { 2,1,3 });
+            #region Tree
+            //var x = DSAPractise.Tree.Tree.BuildTreeFromInorderAndPreorder(new List<int>() { 1,2,3 }, new List<int>() { 2,1,3 });
+
+            #endregion
+
+            #region Stacks
+            var res = Stacks.Stacks.DoubleCharacterTrouble("bcaaaaa");
+            #endregion
 
             Console.ReadLine();
         }
---
+++ DSAPractise/Stacks/Stacks.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPractise.Stacks
+{
+    public static class Stacks
+    {
+        public static int BalancedParanthesis(string A)
+        {
+            Stack<char> stack = new Stack<char>();
+            //loop all the Paranthesis
+            foreach (char c in A)
+            {
+                //if open Paranthesis add to stack
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                //Paranthesis is not open and stack count is 0
+                //means for current Paranthesis there is not open Paranthesis, so not balanced
+                else if (stack.Count == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    //checking for current close Paranthesis, the previous Paranthesis is not matched open Paranthesis
+                    if (c == ')' && stack.Pop() != '(')
+                    {
+                        return 0;
+                    }
+                    //checking for current close Paranthesis, the previous Paranthesis is not matched open Paranthesis
+                    else if (c == '}' && stack.Pop() != '{')
+                    {
+                        return 0;
+                    }
+                    //checking for current close Paranthesis, the previous Paranthesis is not matched open Paranthesis
+                    else if (c == ']' && stack.Pop() != '[')
+                    {
+                        return 0;
+                    }
+                }
+            }
+            //stack count should be 0 because every open Paranthesis should be matched by close Paranthesis
+            return stack.Count == 0 ? 1 : 0;
+        }
+
+        public static string DoubleCharacterTrouble(string A)
+        {
+            Stack<char> stack = new Stack<char>();
+            stack.Push(A[0]);
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (stack.Count == 0 || A[i] != stack.Peek())
+                {
+                    stack.Push(A[i]);
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+            StringBuilder result = new StringBuilder();
+            while (stack.Count > 0)
+            {
+                result.Append(stack.Pop());
+            }
+            return string.Join("", result.ToString().Reverse());
+        }
+
+        public static int EvaluateExpression(List<string> A)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (var a in A)
+            {
+                if (a == "+")
+                {
+                    int v2 = stack.Pop();
+                    int v1 = stack.Pop();
+                    stack.Push(v1 + v2);
+                }
+                else if (a == "-")
+                {
+                    int v2 = stack.Pop();
+                    int v1 = stack.Pop();
+                    stack.Push(v1 - v2);
+                }
+                else if (a == "*")
+                {
+                    int v2 = stack.Pop();
+                    int v1 = stack.Pop();
+                    stack.Push(v1 * v2);
+                }
+                else if (a == "/")
+                {
+                    int v2 = stack.Pop();
+                    int v1 = stack.Pop();
+                    stack.Push(v1 / v2);
+                }
+                else
+                {
+                    stack.Push(int.Parse(a));
+                }
+            }
+            return stack.Pop();
+        }
+
+        public static int PassingGame(int A, int B, List<int> C)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(B);
+            foreach (int c in C)
+            {
+                if (c == 0)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            return stack.Pop();
+        }
+
+        public static List<int> NearestSmallerElement(List<int> A)
+        {
+            List<int> ans = new List<int>();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < A.Count; i++)
+            {
+                while (stack.Count != 0 && stack.Peek() >= A[i])
+                {
+                    stack.Pop();
+                }
+                if (stack.Count == 0)
+                {
+                    ans.Add(-1);
+                }
+                else
+                {
+                    ans.Add(stack.Peek());
+                }
+                stack.Push(A[i]);
+
+            }
+
+            return ans;
+        }
+    }
+}
